5bca7e2de80e0a6adc78c0200899d35edebc850a
JornuthDev <jornuth.dev@gmail.com>
2025-11-25 03:03:59 +0100

    Add files via upload

---
+++ Source/DarkScript/Public/Core/BasePlayerController.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Utils/Types/OnlineTypes.h"
+#include "BasePlayerController.generated.h"
+
+class UProgression;
+/**
+ * 
+ */
+UCLASS()
+class DARKSCRIPT_API ABasePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	// ───────────────────────────────────────────────────────────────────────────────
+	// ─── Construction ──────────────────────────────────────────────────────────────
+	ABasePlayerController();
+	
+	// ───────────────────────────────────────────────────────────────────────────────
+	// ─── Functions (Blueprint API) ─────────────────────────────────────────────────
+	virtual void ClientWasKicked_Implementation(const FText& KickReason) override;
+	
+	// ───────────────────────────────────────────────────────────────────────────────
+	// ─── Functions (Internal) ──────────────────────────────────────────────────────
+	
+	// ───────────────────────────────────────────────────────────────────────────────
+	// ─── Variables ─────────────────────────────────────────────────────────────────
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UProgression> Progression;
+};
---
+++ Source/DarkScript/Public/Utils/Libraries/OnlineLibrary.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "OnlineLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DARKSCRIPT_API UOnlineLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	
+	UFUNCTION(BlueprintPure, Category="Online", meta=(WorldContext="WorldContextObject"))
+	static FString GetPlayerSteamID(const UObject* WorldContextObject, const APlayerController* PlayerController);
+	
+	UFUNCTION(BlueprintPure, Category="Online", meta=(WorldContext="WorldContextObject"))
+	static FString GetPieInstance(const UObject* WorldContextObject, const APlayerController* PlayerController);
+	
+};
