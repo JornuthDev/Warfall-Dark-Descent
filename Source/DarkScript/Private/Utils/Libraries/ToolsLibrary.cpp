ca031b71b910a871d1eed494f2471f3115d6ee76
JornuthDev <jornuth.dev@gmail.com>
2025-12-11 23:24:42 +0100

    Delete Source directory

--- Source/DarkScript/Private/Utils/Libraries/ToolsLibrary.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "Utils/Libraries/ToolsLibrary.h"
-#include "Utils/Tools/ThumbnailMaker.h"
-
-UMaterialInstanceDynamic* UToolsLibrary::ExportMaterial(const FThumbnail Thumbnail, UMaterialInterface* Material,
-                                                        const FThumbnailMaterialParams& Params, const FLinearColor& Color)
-{
-	return Thumbnail.ExportMaterial(Material, Params, Color);
-}
